51faff5eedcda12a92ae1d207b8d34ace6708380
Scott <slangeberg@gmail.com>
2014-05-23 23:10:53 +0100

    Got initial query working against nested latitudes

--- sst-grails/grails-app/controllers/com/greekadonis/sst/SSTDayController.groovy
+++ sst-grails/grails-app/controllers/com/greekadonis/sst/SSTDayController.groovy
@@ -15,6 +15,9 @@ class SSTDayController {
 
     //
     def test = {
+
+        log.info "sstday.count: ${SSTDay.count()}, sstdaylat.count: ${SSTDayLatitude.count()}"
+
         if( SSTDayLatitude.count() > 0 )
             SSTDayLatitude.where{ id != null }.deleteAll()
         if( SSTDay.count() > 0 )
@@ -24,21 +27,31 @@ class SSTDayController {
 //            def longitudes = [
 //                new SSTDayLongitude()
 //            ]
-            SSTDay day = new SSTDay( time: new Date(2006, 04, 03) )
+            SSTDay day = new SSTDay(time: new Date(2006, 04, 03))
+                .addToLatitudes(new SSTDayLatitude(lat: 0.5))
+                .addToLatitudes(new SSTDayLatitude(lat: 25.0))
                 .save(flush: true, failOnError: true)
 
-            def latitude = new SSTDayLatitude(
-                day: day, lat: 20.5/*, longitudes: longitudes*/)
-                    .save(flush: true, failOnError: true)
+//            [
+//                new SSTDayLatitude(day: day, lat: 0.5),
+//                new SSTDayLatitude(day: day, lat: 5.5),
+//                new SSTDayLatitude(day: day, lat: 10.5),
+//                new SSTDayLatitude(day: day, lat: 20.5)
+//
+//            ].saveAll(flush: true, failOnError: true)
+
+//            def latitude = new SSTDayLatitude(
+//                day: day, lat: 20.5/*, longitudes: longitudes*/)
+//                .save(flush: true, failOnError: true)
 //            day.latitudes = [
 //                latitude
 //            ]
         }
 
         def query = SSTDayLatitude.where {
-            lat > 30
+            lat > 0 && lat < 30
         }
 
-        render query.find()
+        render query.findAll()
     }
 }
